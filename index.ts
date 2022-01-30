1db738ac81c5adca52ae3a730e2f6815d82e8c0a
Jang-oi <jjh53241@gmail.com>
2022-01-30 21:42:11 +0900

    Part 1 : 꼭 알아야할 내용  - 타입스크립트 기본 타입 정리 (primitive types)

--- index.ts
+++ index.ts
@@ -1,23 +1,18 @@
-type Name = string | number
+let 결혼했니 :boolean = false;
 
-let 이름 : Name = '123';
+let 회원들 :string[] = ['kim', 'park']
 
-// 파라미터로는 number 가 들어와야되고 return 도 number 로 해야함.
-function haha(x : number) : number {
-    return x * 2;
+let 내정보 : { age : number } = { age : 20 }
+
+let 이름 = 'kim';
+let 나이 = 20;
+
+let project :{
+    member : string[],
+    days : number,
+    started : boolean,
+} = {
+    member : ['kim', 'park'],
+    days : 30,
+    started : true,
 }
-
-// Member 라는 type 은 name 이란 키에 string 으로 가져야함.
-type Member = {
-    name : string
-}
-
-// Member2 라는 type 은 모든 object 속성이 string 이여야함.
-type Member2 = {
-    [key : string] : string
-}
-
-let john : Member2 = { name : 'kim', age : '123'};
-
-
-
